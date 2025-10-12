d7dfdcf7043a7c0c8c6bfa985644cf5f314c6ad2
Святослав <slava.minasyan2006@mail.ru>
2025-10-12 12:37:36 +0300

    feat: Removed unnecessary files

--- mg-profile-service-common/src/main/java/ru/solomka/profile/common/EntityNotification.java
+++
@@ -1,7 +0,0 @@
-package ru.solomka.profile.common;
-
-public interface EntityNotification<M, E> {
-    void notifyCreate(M message, E entity);
-    void notifyUpdate(M message, E entity);
-    void notifyDelete(M message, E entity);
-}
