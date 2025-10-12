a719f118a0f6a4dce1a730b15ae88518fc7e8058
Святослав <slava.minasyan2006@mail.ru>
2025-10-12 12:30:37 +0300

    feat: Added kafka receiver notification adapter

---
+++ mg-profile-service-kafka-adapter/src/main/java/ru/solomka/profile/kafka/event/KafkaUserDeletedEvent.java
@@ -0,0 +1,16 @@
+package ru.solomka.profile.kafka.event;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import ru.solomka.profile.principal.PrincipalEntity;
+import ru.solomka.profile.user.UserSnapshotEntity;
+
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+@AllArgsConstructor
+@NoArgsConstructor(force = true)
+@Builder
+@Setter @Getter
+public class KafkaUserDeletedEvent extends KafkaEvent<UserSnapshotEntity, PrincipalEntity> {
+    @NonNull UserSnapshotEntity firstChapterNotifyMessage;
+    @NonNull PrincipalEntity secondChapterNotifyMessage;
+}
