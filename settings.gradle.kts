c798edc3429cf311da8c910859c1819cec121e4f
Святослав <slava.minasyan2006@mail.ru>
2025-10-12 12:28:18 +0300

    feat: Added dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,6 +8,15 @@ listOf(
 
     "mg-profile-service-principal",
     "mg-profile-service-principal-spring-security-adapter",
+
+    "mg-profile-service-kafka-adapter",
+
+    "mg-profile-service-profile",
+    "mg-profile-service-profile-spring-jpa-adapter",
+
+    "mg-profile-service-user-snapshot",
+    "mg-profile-service-user-snapshot-spring-jpa-adapter"
 ).forEach {
     include(it)
 }
+include("mg-profile-service-profile-spring-rest-adapter")
