1bf9dbf16ed158d0d0b7ea8a8f7a2e700ad0b423
Святослав <slava.minasyan2006@mail.ru>
2025-10-13 21:44:40 +0300

    feat: Minor changes

--- mg-profile-service-profile-spring-jpa-adapter/src/main/java/ru/solomka/profile/profile/CrudProfileServiceAdapter.java
+++ mg-profile-service-profile-spring-jpa-adapter/src/main/java/ru/solomka/profile/profile/CrudProfileServiceAdapter.java
@@ -2,45 +2,52 @@ package ru.solomka.profile.profile;
 
 import lombok.AccessLevel;
 import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import ru.solomka.profile.common.BaseCrudRepository;
 import ru.solomka.profile.common.BaseJpaRepositoryAdapter;
 import ru.solomka.profile.common.exception.EntityNotFoundException;
 import ru.solomka.profile.common.mapper.Mapper;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class CrudProfileServiceAdapter extends BaseJpaRepositoryAdapter<JpaProfileEntity, ProfileEntity> implements ProfileRepository {
 
     @NonNull CrudProfileRepository crudProfileRepository;
-
+    @NonNull Mapper<JpaProfileEntity, ProfileEntity>  jpaProfileEntityProfileEntityMapper;
 
     public CrudProfileServiceAdapter(@NonNull CrudProfileRepository repository,
                                      @NonNull Mapper<JpaProfileEntity, ProfileEntity> mapper) {
         super(repository, mapper);
         this.crudProfileRepository = repository;
+        this.jpaProfileEntityProfileEntityMapper = mapper;
     }
 
     @Override
     public ProfileEntity getProfileByProfileName(@NonNull String profileName) {
         return crudProfileRepository.findProfileByProfileName(profileName)
+                .map(jpaProfileEntityProfileEntityMapper::mapToDomain)
                 .orElseThrow(() -> new EntityNotFoundException(String.format("Profile with profile name '%s' not found", profileName)));
     }
 
     @Override
     public List<ProfileEntity> getProfilesByFirstName(@NonNull String firstName) {
-        return crudProfileRepository.findProfilesByFirstName(firstName);
+        return crudProfileRepository.findProfilesByFirstName(firstName).stream()
+                .map(jpaProfileEntityProfileEntityMapper::mapToDomain)
+                .collect(Collectors.toList());
     }
 
     @Override
     public List<ProfileEntity> getProfilesByLastName(@NonNull String lastName) {
-        return crudProfileRepository.findProfilesByLastName(lastName);
+        return crudProfileRepository.findProfilesByLastName(lastName).stream()
+                .map(jpaProfileEntityProfileEntityMapper::mapToDomain)
+                .collect(Collectors.toList());
     }
 
     @Override
     public List<ProfileEntity> getProfilesByFirstNameAndLastName(@NonNull String firstName, @NonNull String lastName) {
-        return crudProfileRepository.findProfilesByFirstNameAndLastName(firstName, lastName);
+        return crudProfileRepository.findProfilesByFirstNameAndLastName(firstName, lastName).stream()
+                .map(jpaProfileEntityProfileEntityMapper::mapToDomain)
+                .collect(Collectors.toList());
     }
 }
