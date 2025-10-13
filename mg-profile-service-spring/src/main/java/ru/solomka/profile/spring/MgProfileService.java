acc7683320eccfa6a36ffa70ca0e11d323ee1d01
Святослав <slava.minasyan2006@mail.ru>
2025-10-13 21:44:44 +0300

    feat: Minor changes

--- mg-profile-service-spring/src/main/java/ru/solomka/profile/spring/MgProfileService.java
+++ mg-profile-service-spring/src/main/java/ru/solomka/profile/spring/MgProfileService.java
@@ -4,7 +4,7 @@ package ru.solomka.profile.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "ru.solomka.profile")
 public class MgProfileService {
     public static void main(String[] args) {
         SpringApplication.run(MgProfileService.class, args);
